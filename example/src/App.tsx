0812040c7006608178d45cb349342fa3adb2fc7c
Andrei Telteu <andrei.telteu@touch-media.ro>
2023-01-07 23:46:50 +0200

    Installed solidjs in example folder

---
+++ example/src/App.tsx
@@ -0,0 +1,15 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+    return (
+        <div class="App">
+            <header class="header">
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+            </header>
+        </div>
+    );
+};
+
+export default App;
